2df214f944ecfe35c241cdcfa6af46e30515b682
Dayana Ilieva <dayana.ilieva@1forfit.com>
2021-12-29 12:49:10 +0200

    add comments on code

--- modal.js
+++ modal.js
@@ -14,3 +14,11 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+// to avoid repetition in our code we can use element.classList.toggle("mystyle"); 
+// you can take a look in MDN (https://developer.mozilla.org/) about toggle and how to use it.
+
+// bare in mind when selecting document.getElementsByClassName("close")[0] like this there is a chance that the array could change, so the index may be different
+// for selectors of HTML elements I use:
+// document.querySelectorAll('.close')[0]
+// let modal = document.querySelector("#myModal"); (you may find it easier) and if you want to select multiple querySelectorAll('elementClass'/'elementID')
